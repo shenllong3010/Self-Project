74b728b9c9f3a60af3940492ec0b5414a0e295d0
Loc Lam <shenlong3010@gmail.com>
2020-09-16 21:23:28 -0400

    fix the game with better algorithm and better performance

--- number-guessing/main.py
+++ number-guessing/main.py
@@ -1,24 +1,35 @@
-import random
+import random, math
+# O(n) time/ O(log2) space
 
-guesses = 0
-
-selected_range = input("Range min-max: ").split()
-selected_range.sort()
-
-computer_num = random.randint(int(selected_range[0]), int(selected_range[1]))
-
-print(computer_num)
-while True:
+def main():
     
-    guess_num = int(input('What is your number: '))
+    # Minimum number of guesses = log2(upper - lower + 1)
+    lower = int(input("Enter lower bound: "))
+    upper = int(input("Enter upper bound: "))
 
-    if guess_num != computer_num and guess_num > computer_num:
-        guesses += 1
-        print("Try Again! You guessed too high")
-    elif guess_num != computer_num and guess_num < computer_num:
-        guesses += 1
-        print("Try Again! You guessed too small")
-    else:
-        guesses += 1
-        print(f"You win with {guesses} guesses")
-        break
+    computer_num = random.randint(lower, upper)
+    max_num_guesses = math.log(upper - lower + 1, 2) # second arg for log base
+    print(f"\n\tYou've only {round(max_num_guesses)} chances to guess the number!\n")
+
+    guess = 0
+    while guess < max_num_guesses:
+
+        guess_num = int(input('Guess a number: '))
+        guess +=1
+        
+        if computer_num == guess_num:
+            print(f"Congratulations you did it in {guess} try")
+            break
+        
+        elif computer_num > guess_num:
+            print("You guess too small!")
+            
+        elif computer_num < guess_num:
+            print("You guess too high!")
+
+    if guess >= max_num_guesses:
+        print(f"\nThe number is {computer_num}")
+        print("\tBetter Luck Next Time!")
+    
+if __name__ == "__main__":
+    main()
